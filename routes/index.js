e701904d7104a465ba1ca3ecf6562d8f532bd64d
Justin Walters <jw3043@columbia.edu>
2015-02-12 15:46:27 -0500

    Added form to submit new tweets

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 // could use one line instead: var router = require('express').Router();
 
 var tweetBank = require('../tweetBank');
 
+// parse application/x-www-form-urlencoded
+var jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+router.post('/submit', urlencodedParser, function(req, res, next){
+	if (!req.body) return res.sendStatus(400);
+	next();
+});
+
+router.post('/submit', function(req, res) {
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
+});
+
 router.get('/', function (req, res) {
   var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+  res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true } );
 });
 
 router.get('/users/:name', function(req, res) {
