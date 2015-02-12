878e6e5c39ba2d5ab85c1c0c3bc2b43cedeeeafd
Justin Walters <jw3043@columbia.edu>
2015-02-12 14:11:58 -0500

    Added user profile

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,12 @@ router.get('/', function (req, res) {
   res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
 });
 
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  res.render( 'index', { title: 'Twitter.js - Posts by ' + name, tweets: list } );
+});
+
 router.get('/*', function(req, res){
 	res.send('Not a path');
 });
