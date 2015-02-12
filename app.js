44718443aa247873f0413198f395331f692dc5c9
Justin Walters <jw3043@columbia.edu>
2015-02-12 01:33:16 -0500

    Improved template, style, routing

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require( 'express' );
 var morgan = require('morgan');
 var swig = require('swig');
+var routes = require('./routes/');
 
 var app = express();
 
+app.use('/', routes);
 app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
@@ -13,13 +16,4 @@ swig.setDefaults({ cache: false });
 
 app.listen( 3000, function(){
 	console.log('server listening');
-});
-
-app.get('/', function(req, res){
-	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-	res.render( 'index', {title: 'Hall of Fame', people: people} );
-});
-
-app.get('/*', function(req, res){
-	res.send('Not a path');
 });
