dd730c8655eb0e87af2f944c230d36bdbe30d478
Justin Walters <jw3043@columbia.edu>
2015-02-12 01:11:52 -0500

    Added basic template

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 var express = require( 'express' );
 var morgan = require('morgan');
+var swig = require('swig');
 
 var app = express();
 
 app.use(morgan('dev'));
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+swig.setDefaults({ cache: false });
 
 app.listen( 3000, function(){
 	console.log('server listening');
 });
 
 app.get('/', function(req, res){
-	res.send('Hey');
+	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+	res.render( 'index', {title: 'Hall of Fame', people: people} );
+});
+
+app.get('/*', function(req, res){
+	res.send('Not a path');
 });
